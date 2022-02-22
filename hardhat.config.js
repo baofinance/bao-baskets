613a4eaa50e54155a4ea0e6ca322c080c4956883
Marlon <marlon.bittner@gmail.com>
2022-02-22 10:32:58 +0200

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,48 @@
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+ module.exports = {
+  networks: {
+    localhost: {
+      //Requires start of local network at port:
+      url: "http://127.0.0.1:8545"
+    },
+    hardhat: {},
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.7.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        },
+      },
+      {
+        version: "0.8.1",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+        },
+      },
+      {
+        version: "0.6.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          },
+        },
+      },
+    ],
+  },
+  mocha: {
+    timeout: 10000000
+  }
+};
