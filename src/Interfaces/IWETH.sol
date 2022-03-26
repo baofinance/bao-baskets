ef246cd8b9e20c781504374c5041fb1636c5982d
N0xMare <cjusticemace@gmail.com>
2022-03-25 22:29:27 -0400

    forge install: ds-test

--- src/Interfaces/IWETH.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity 0.7.1;
-pragma experimental ABIEncoderV2;
-
-import "./IERC20.sol";
-
-interface IWETH is IERC20 {
-    function withdraw(uint wad) external;
-}
--- src/Recipes/Interfaces/IWETH.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.8.1;
-
-import "./IERC20.sol";
-
-interface IWETH is IERC20 {
-    function withdraw(uint wad) external;
-}
