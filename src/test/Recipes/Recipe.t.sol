8907470b5f7f598b1961ace2c8f77a271c8703d0
Marlon <marlon.bittner@gmail.com>
2022-03-30 18:16:51 +0200

    initial mint test

--- src/test/Recipes/Recipe.t.sol
+++ src/test/Recipes/Recipe.t.sol
@@ -10,4 +10,24 @@ contract RecipeTest is DSTest {
     function setUp() public {
         testSuite = new BasketsTestSuite();
     }
+
+    function testMint() public {
+        Recipe recipe = testSuite.recipe();
+        IERC20(testSuite.basket()).approve(address(recipe),type(uint256).max);
+        uint[] memory mintAmounts = new uint[](3);
+        mintAmounts[0] = 0;
+        mintAmounts[1] = 100;
+        mintAmounts[2] = 1e18;
+
+        for (uint256 i = 0; i < mintAmounts.length; i++) {
+            (uint256 mintPrice, uint16[] memory dexIndex) = recipe.getPricePie(testSuite.basket(), mintAmounts[i]);
+
+            recipe.bake(
+            testSuite.basket(),
+            mintPrice,
+            mintAmounts[i],
+            _dexIndex
+            );
+        }
+    }
 }
