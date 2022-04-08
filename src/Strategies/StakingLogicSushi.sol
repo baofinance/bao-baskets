5c530c698c362624673d782a0ecc63c537ed854e
FreshPizza <fpizza@protonmail.com>
2022-04-08 15:00:58 +0200

    Removing solmate submodule...apparently they are to good for ^0.7.0

--- src/Strategies/StakingLogicSushi.sol
+++ src/Strategies/StakingLogicSushi.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.7.1;
 import "../LendingRegistry.sol";
 import "../Interfaces/IXSushi.sol";
 import "@openzeppelin/token/ERC20/IERC20.sol";
+import { mulDivUp } from "@solmate/utils/FixedPointMathLib.sol";
 
 contract StakingLogicSushi{
 
@@ -70,26 +71,4 @@ contract StakingLogicSushi{
         IERC20 xToken = IERC20(_wrapped);
         IERC20 token = IERC20(lendingRegistry.wrappedToUnderlying(_wrapped));
         return mulDivUp(token.balanceOf(_wrapped),10**18,xToken.totalSupply());
-    }
-    
-    function mulDivUp(
-        uint256 x,
-        uint256 y,
-        uint256 denominator
-    ) internal pure returns (uint256 z) {
-        assembly {
-            // Store x * y in z for now.
-            z := mul(x, y)
-
-            // Equivalent to require(denominator != 0 && (x == 0 || (x * y) / x == y))
-            if iszero(and(iszero(iszero(denominator)), or(iszero(x), eq(div(z, x), y)))) {
-                revert(0, 0)
-            }
-
-            // First, divide z - 1 by the denominator and add 1.
-            // We allow z - 1 to underflow if z is 0, because we multiply the
-            // end result by 0 if z is zero, ensuring we return 0 if z is zero.
-            z := mul(iszero(iszero(z)), add(div(sub(z, 1), denominator), 1))
-        }
-    }
-}
+    } 
